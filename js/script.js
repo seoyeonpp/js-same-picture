0c4d0fa4614c1cde86882e105c3a15e442bc5e95
seoyeonpp <seoyeonpub@gmail.com>
2021-12-20 23:00:16 +0900

    스크립트, scss 추가

--- js/script.js
+++ js/script.js
@@ -1,13 +1,34 @@
 (function(window){
     samePicture = {
         init : function(){
-            console.log('dldl');
+            samePicture.imgClick();
         },
         imgClick : function(){
-            document.getElementsByClassName('img').addEventListener('click',function(){
-                console.log('click');
-            });
+            const imgTypeA = document.getElementById('typeA'),
+                    imgA = imgTypeA.children,
+                    imgTypeB = document.getElementById('typeB'),
+                    imgB = imgTypeB.children;
+
+            // imgA
+            for (var i = 0; i < imgA.length; i++){
+                let eachImg = imgA[i];
+                eachImg.addEventListener('click',function(){
+                    this.className += ' active';
+                    console.log(this.getAttribute('data-img'));
+                });
+            };
+
+            // imgB
+            for (var i = 0; i < imgB.length; i++){
+                let eachImg = imgB[i];
+                eachImg.addEventListener('click',function(){
+                    this.className += ' active';
+                    console.log(this.getAttribute('data-img'));
+                });
+            };
         },
+        // data-img값을 리턴 받아서 둘이 같으면 correct 클래스 붙기
+    
     }
     return samePicture.init();
 })(window);
